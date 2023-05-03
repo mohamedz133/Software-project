8b6a35da507bf82c03e3b79ac8b2d034f7848566
abohend <mhmdabohend@gmail.com>
2023-05-03 17:21:15 +0300

    connected to DB

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,30 @@
 import express from "express";
+import dotenv from "dotenv"; 
+import morgan from "morgan";
+import mongoose from "mongoose";
 
+dotenv.config({path: './config.env'}); // load .env file
+
+
+// Connect to DB
+mongoose.connect(process.env.DB_URI).then(conn => {
+    console.log(`DB connected with host: ${conn.connection.host}`);
+})
+.catch(err => {
+    console.log(`DB connection failed: ${err}`);
+    process.exit(1);
+})
+
+
+// Initialize express
 const app = express();
+
+
+// Dev logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
 
 
 app.get("/", (req,res) => {
@@ -8,6 +32,7 @@ app.get("/", (req,res) => {
 });
 
 
-app.listen(8000, () => {
-    console.log("Listening on port 8000")
+const port = process.env.PORT  || 8000;
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
