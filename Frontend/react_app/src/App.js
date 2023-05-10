08283acf3fd2a286aac4bdab23121438db1afa13
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-05-10 21:02:44 +0300

    added About page prototype

--- Frontend/react_app/src/App.js
+++ Frontend/react_app/src/App.js
@@ -1,15 +1,15 @@
-
-
 import './App.css';
 import Login from "./pages/Login.js";
 import Signup from "./pages/signup";
 import About from "./pages/About";
+import Navigation from "./components/Navigation";
+import {Navigate} from "react-router-dom";
 
 
 function App() {
     return (
         <div className="App">
-        <About/>
+        <Navigation />
         </div>
     );
 }
