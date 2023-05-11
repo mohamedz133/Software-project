16f1a90d59c9e542615274b942a59df04211a9f3
Ibrahim Elsheikh <77652042+ibrahelsheikh@users.noreply.github.com>
2023-05-11 05:17:37 +0300

    About.js page react js

--- Frontend/react_app/src/App.js
+++ Frontend/react_app/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Contactus from "./pages/Contactus";
 import Home from "./pages/Home";
+import About from "./pages/About";
 
 
 function App() {
     return (
         <div className="App">
-        <Home />
+        <About/>
         </div>
     );
 }
